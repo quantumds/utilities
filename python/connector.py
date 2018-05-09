9ba0306b0d5eaa8c846d269de45e22e471ec9dc1
quantumds <19568551+quantumds@users.noreply.github.com>
2018-05-09 10:31:29 +0200

    Delete connector.py

--- python/connector.py
+++
@@ -1,5 +0,0 @@
-############################################################################
-# IMPORT DATA
-############################################################################
-plms = pd.read_csv(file_dir + file_name_plms, sep = ',', header = 0, encoding = 'latin-1', low_memory = False)
-dqss = pd.read_csv(file_dir + file_name_dqss, sep = ',', header = 0, encoding = 'latin-1', low_memory = False)
