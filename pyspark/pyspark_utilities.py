75059cbf4cbc2ed95fecd7ce16403dcc0327424e
quantumds <19568551+quantumds@users.noreply.github.com>
2018-05-15 14:33:20 +0200

    Update pyspark_utilities.py

--- pyspark/pyspark_utilities.py
+++ pyspark/pyspark_utilities.py
@@ -2,8 +2,12 @@
 df = spark.read.parquet(route_of_your_parquet_file)
 # OBTAIN HEADER
 df.show(n=4) # n = number of lines that you want to see in the header of the file
+
 # OBTAIN COLUMN TYPES
 df.printSchema()
+# OBTAIN COLUMN NAMES
+spark_df.schema.names
 df.dtypes # Does the same
+
 # VIEW DATAFRAMES
 display(df)
