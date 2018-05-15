2c9cd4e102b91ab0d50f5a7579477af13c761385
quantumds <19568551+quantumds@users.noreply.github.com>
2018-05-15 10:36:37 +0200

    Update pyspark_utilities.py

--- pyspark/pyspark_utilities.py
+++ pyspark/pyspark_utilities.py
@@ -1,4 +1,6 @@
 # LOAD PARQUET FILES
 df = spark.read.parquet(route_of_your_parquet_file)
+# OBTAIN HEADER
+df.show(n=4) # n = number of lines that you want to see in the header of the file
 # OBTAIN COLUMN TYPES
 df.printSchema()
