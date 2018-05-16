3405d3b58b439db1cba40f5c0356cf65b5199980
quantumds <19568551+quantumds@users.noreply.github.com>
2018-05-16 15:31:23 +0200

    Update pyspark_utilities.py

--- pyspark/pyspark_utilities.py
+++ pyspark/pyspark_utilities.py
@@ -20,6 +20,8 @@ df = df.withColumn("name_of_column", df["name_of_column"].cast(StringType()))
 to_str = ['variable_to_str_1','variable_to_str_2','variable_to_str_3', ...]
 for col in to_str:
   df = df.withColumn(col, df[col].cast(StringType()))
+# Same as the for expression but in one-liner:
+perf = perf.select([col(c).cast(StringType()).alias(c) for c in perf_to_str])
   
 # ACTIVATE MARKDOWN COMMANDS
 %md
