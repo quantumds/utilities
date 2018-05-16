fd13ab359582bdff30f6eeb2fc173fe900a12d70
quantumds <19568551+quantumds@users.noreply.github.com>
2018-05-16 13:21:12 +0200

    Update pyspark_utilities.py

--- pyspark/pyspark_utilities.py
+++ pyspark/pyspark_utilities.py
@@ -8,7 +8,24 @@ df.printSchema()
 df.dtypes # Does the same
 
 # OBTAIN COLUMN NAMES
-spark_df.schema.names
+df.schema.names
 
 # VIEW DATAFRAMES
 display(df)
+
+# DATA TYPE CONVERSION
+from pyspark.sql.types import IntegerType
+df = df.withColumn("name_of_column", df["name_of_column"].cast(StringType()))
+# Convert several columns to String:
+to_str = ['variable_to_str_1','variable_to_str_2','variable_to_str_3', ...]
+for col in to_str:
+  df = df.withColumn(col, df[col].cast(StringType()))
+  
+# ACTIVATE MARKDOWN COMMANDS
+%md
+type_your_markdown_lines
+[close_the_cell_you_are_in]
+
+
+
+
