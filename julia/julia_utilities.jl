cdd5b2f41dc586244f3d559101475517bd5a6913
quantumds <19568551+quantumds@users.noreply.github.com>
2018-09-04 21:02:06 +0200

    Update julia_utilities.jl

--- julia/julia_utilities.jl
+++ julia/julia_utilities.jl
@@ -1,3 +1,8 @@
+# GENERAL KNOWLEDGE
+# Single quotes are reserved to characters.
+# Double quotes are reserved to strings.
+# Julia does not permit operators (+, -, *, ...), "methods" in Julia; that are not previously defined.
+
 # PRINT THROUGH THE SCREEN
 println(object_to_print)
 
