49ef8569b3e4157c9c332e355a94eb29a90bb8a0
quantumds <19568551+quantumds@users.noreply.github.com>
2018-09-11 20:09:15 +0200

    Update sql_utilities.sql

--- sql/sql_utilities.sql
+++ sql/sql_utilities.sql
@@ -1,16 +1,24 @@
-# SELECT INFORMATION FROM TABLE:
-# Select all columns:
-select * from tablename; /*Select all columns from a table */
-# Select one column:
-select name_of_column from table;
-# Select several columns from table:
-select column_1, column_2 from table table_name;
+-- SELECT INFORMATION FROM TABLE:
+-- Select all columns:
+SELECT * 
+FROM tablename; /*Select all columns from a table */
+-- Select one column:
+SELECT name_of_column 
+FROM table;
+-- Select several columns from table:
+SELECT column_1, column_2 
+FROM table_name;
 
-# CREATE A TABLE COPY FROM ANOTHER ONE
+-- WHERE
+SELECT *
+FROM table_name
+WHERE column_name = column_value;
+
+-- CREATE A TABLE COPY FROM ANOTHER ONE
 SELECT *
     INTO db.new_table
   FROM db.old_table
   
-# ADD A COLUMN TO A TABLE
+-- ADD A COLUMN TO A TABLE
 ALTER TABLE table_name
   ADD column_name column_TYPE;
