04abb9d3909527909b0ccd939be20ba47f138602
quantumds <19568551+quantumds@users.noreply.github.com>
2019-02-18 15:38:52 +0100

    Update python_utilities.py

--- python/python_utilities.py
+++ python/python_utilities.py
@@ -450,3 +450,447 @@ plms[to_cat] = plms[to_cat].apply(lambda x: x.astype('category'))
 
 # PRINT CSV TABLE / EXPORT CSV TABLE / PRINT TEXT FILES / EXPORT
 df.to_csv(directory_with_slash_at_end + name_of_file_with_extension, sep='|', index = False)
+
+# ADDING VERTICAL LINES TO PLOTS
+# ys is the value where we want the line to be marked.
+def axhlines(ys, ax=None, **plot_kwargs):
+    """
+    Draw horizontal lines across plot
+    :param ys: A scalar, list, or 1D array of vertical offsets
+    :param ax: The axis (or none to use gca)
+    :param plot_kwargs: Keyword arguments to be passed to plot
+    :return: The plot object corresponding to the lines.
+    """
+    if ax is None:
+        ax = plt.gca()
+    ys = np.array((ys, ) if np.isscalar(ys) else ys, copy=False)
+    lims = ax.get_xlim()
+    y_points = np.repeat(ys[:, None], repeats=3, axis=1).flatten()
+    x_points = np.repeat(np.array(lims + (np.nan, ))[None, :], repeats=len(ys), axis=0).flatten()
+    plot = ax.plot(x_points, y_points, scalex = False, **plot_kwargs)
+    return plot
+
+# ADDING HORIZONTAL LINES TO PLOTS
+# xs is the value where we want the line to be marked.
+def axvlines(xs, ax=None, **plot_kwargs):
+    """
+    Draw vertical lines on plot
+    :param xs: A scalar, list, or 1D array of horizontal offsets
+    :param ax: The axis (or none to use gca)
+    :param plot_kwargs: Keyword arguments to be passed to plot
+    :return: The plot object corresponding to the lines.
+    """
+    if ax is None:
+        ax = plt.gca()
+    xs = np.array((xs, ) if np.isscalar(xs) else xs, copy=False)
+    lims = ax.get_ylim()
+    x_points = np.repeat(xs[:, None], repeats=3, axis=1).flatten()
+    y_points = np.repeat(np.array(lims + (np.nan, ))[None, :], repeats=len(xs), axis=0).flatten()
+    plot = ax.plot(x_points, y_points, scaley = False, **plot_kwargs)
+    return plot
+def scatter_matrix_all(frame, alpha=0.5, figsize=None, grid=False, diagonal='hist', marker='.', density_kwds=None, hist_kwds=None, range_padding=0.05, **kwds):
+
+    import matplotlib as mpl
+    import matplotlib.pyplot as plt
+    from matplotlib.artist import setp
+    import pandas.core.common as com
+    from pandas.compat import range, lrange, lmap, map, zip
+    from statsmodels.nonparametric.smoothers_lowess import lowess
+    
+    df = frame
+    num_cols = frame._get_numeric_data().columns.values
+    n = df.columns.size
+    fig, axes = plt.subplots(nrows=n, ncols=n, figsize=figsize, squeeze=False)
+
+    # no gaps between subplots
+    fig.subplots_adjust(wspace=0, hspace=0)
+
+    mask = com.notnull(df)
+    marker = _get_marker_compat(marker)
+
+    hist_kwds = hist_kwds or {}
+    density_kwds = density_kwds or {}
+
+    # workaround because `c='b'` is hardcoded in matplotlibs scatter method
+    kwds.setdefault('c', plt.rcParams['patch.facecolor'])
+
+    boundaries_list = []
+    for a in df.columns:
+        if a in num_cols:
+            values = df[a].values[mask[a].values]
+        else:
+            values = df[a].value_counts()
+        rmin_, rmax_ = np.min(values), np.max(values)
+        rdelta_ext = (rmax_ - rmin_) * range_padding / 2.
+        boundaries_list.append((rmin_ - rdelta_ext, rmax_+ rdelta_ext))
+
+    for i, a in zip(lrange(n), df.columns):
+        for j, b in zip(lrange(n), df.columns):
+            ax = axes[i, j]
+
+            if i == j:
+                if a in num_cols:    # numerical variable
+                    values = df[a].values[mask[a].values]
+                    # Deal with the diagonal by drawing a histogram there.
+                    if diagonal == 'hist':
+                        ax.hist(values, **hist_kwds)
+                    elif diagonal in ('kde', 'density'):
+                        from scipy.stats import gaussian_kde
+                        y = values
+                        gkde = gaussian_kde(y)
+                        ind = np.linspace(y.min(), y.max(), 1000)
+                        ax.plot(ind, gkde.evaluate(ind), **density_kwds)
+                    ax.set_xlim(boundaries_list[i])
+                else:                # categorical variable
+                    values = df[a].value_counts()
+                    ax.bar(list(range(df[a].nunique())), values)
+            else:
+                common = (mask[a] & mask[b]).values
+                # two numerical variables
+                if a in num_cols and b in num_cols:
+                    if i > j:
+                        ax.scatter(df[b][common], df[a][common], marker=marker, alpha=alpha, **kwds)
+                        # The following 2 lines add the lowess smoothing
+                        ys = lowess(df[a][common], df[b][common])
+                        ax.plot(ys[:,0], ys[:,1], 'red')
+                    else:
+                        pearR = df[[a, b]].corr()
+                        ax.text(df[b].min(), df[a].min(), 'r = %.4f' % (pearR.iloc[0][1]))
+                    ax.set_xlim(boundaries_list[j])
+                    ax.set_ylim(boundaries_list[i])
+                # two categorical variables
+                elif a not in num_cols and b not in num_cols:
+                    if i > j:
+                        from statsmodels.graphics import mosaicplot
+                        mosaicplot.mosaic(df, [b, a], ax, labelizer=lambda k:'')
+                # one numerical variable and one categorical variable
+                else:
+                    if i > j:
+                        tol = pd.DataFrame(df[[a, b]])
+                        if a in num_cols:
+                            label = [ k for k, v in tol.groupby(b) ]
+                            values = [ v[a].tolist() for k, v in tol.groupby(b) ]
+                            ax.boxplot(values, labels=label)
+                        else:
+                            label = [ k for k, v in tol.groupby(a) ]
+                            values = [ v[b].tolist() for k, v in tol.groupby(a) ]
+                            ax.boxplot(values, labels=label, vert=False)
+
+            ax.set_xlabel('')
+            ax.set_ylabel('')
+
+            _label_axis(ax, kind='x', label=b, position='bottom', rotate=True)
+            _label_axis(ax, kind='y', label=a, position='left')
+
+            if j!= 0:
+                ax.yaxis.set_visible(False)
+            if i != n-1:
+                ax.xaxis.set_visible(False)
+
+    for ax in axes.flat:
+        setp(ax.get_xticklabels(), fontsize=8)
+        setp(ax.get_yticklabels(), fontsize=8)
+    return fig
+    
+
+def _label_axis(ax, kind='x', label='', position='top', ticks=True, rotate=False):
+    from matplotlib.artist import setp
+    if kind == 'x':
+        ax.set_xlabel(label, visible=True)
+        ax.xaxis.set_visible(True)
+        ax.xaxis.set_ticks_position(position)
+        ax.xaxis.set_label_position(position)
+        if rotate:
+            setp(ax.get_xticklabels(), rotation=90)
+    elif kind == 'y':
+        ax.yaxis.set_visible(True)
+        ax.set_ylabel(label, visible=True)
+        #ax.set_ylabel(a)
+        ax.yaxis.set_ticks_position(position)
+        ax.yaxis.set_label_position(position)
+    return
+
+def _get_marker_compat(marker):
+    import matplotlib.lines as mlines
+    import matplotlib as mpl
+    if mpl.__version__ < '1.1.0' and marker == '.':
+        return 'o'
+    if marker not in mlines.lineMarkers:
+        return 'o'
+    return marker
+
+############################################################################
+# PRE-PROCESSING FUNCTIONS
+############################################################################
+# Frequency table of every variable:
+def freq(df):
+    pd.options.display.max_rows = 10
+    for col in list(df):
+        print(col, flush = True)    
+        above_zero = df[col].value_counts()
+        print(above_zero[above_zero > 0])
+        print('')
+        print('---------------------------------------------------------------------------')
+        print('')
+    return print('')
+
+# Frequency table of each categorical variable:
+def freqcat(df):
+  pd.options.display.max_rows = 10
+  categories = list(df.select_dtypes(['category']))
+  booleans = list(df.select_dtypes(['bool']))
+  concat = categories + booleans
+  for col in concat:
+    print(col, flush = True)  
+    above_zero = df[col].value_counts()
+    print(above_zero[above_zero > 0])
+    print('')
+    print('---------------------------------------------------------------------------')
+    print('')
+  return print('')
+
+# Frequency table of every variable in long format:
+def lfreq(df):
+    pd.options.display.max_rows = 1000
+    for col in list(df):
+        print(col, flush = True)
+        above_zero = df[col].value_counts()
+        print(above_zero[above_zero > 0])
+        print('')
+        print('---------------------------------------------------------------------------')
+        print('')
+    return print('')
+
+# Frequency table of each categorical variable in long format:
+def lfreqcat(df):
+  pd.options.display.max_rows = 1000
+  categories = list(df.select_dtypes(['category']))
+  booleans = list(df.select_dtypes(['bool']))
+  objects = list(df.select_dtypes(['object']))
+  concat = categories + booleans
+  for col in concat:
+    print(col, flush = True)
+    above_zero = df[col].value_counts()
+    print(above_zero[above_zero > 0])
+    print('')
+    print('---------------------------------------------------------------------------')
+    print('')
+  return print('')
+
+# Convert to factors:
+def to_category(df):
+  df[to_cat] = df[to_cat].apply(lambda x: x.astype('category'))
+  return df
+
+# Convert to numeric: 
+def to_numeric(df):
+  df[to_num] = df[to_num].apply(pd.to_numeric, errors='coerce')
+  return df
+
+# Convert dates in format %b-%Y to numeric: 
+def date_to_numeric(df):
+  for col in dates:
+    print("Converting date variables to years: " + col, flush = True)
+    df[col] = df[col].apply(pd.to_datetime, format = '%b-%Y', errors = 'coerce')     
+    df[col] = 2018 - df[col].apply(pd.to_datetime, errors = 'coerce').dt.year
+  return df
+
+# Convert to string:
+def to_string(df):
+  df[to_str] = df[to_str].apply(lambda x: x.astype(str))
+  return df
+
+def numlevels(df):
+    print('CATEGORY')
+    print('---------------------------------------------------------------------------')
+    print(df.select_dtypes(['category']).apply(lambda x: len(set(x))), flush = True)  
+    print('')
+    print('BOOLEANS')
+    print('---------------------------------------------------------------------------')
+    print(df.select_dtypes(['bool']).apply(lambda x: len(set(x))), flush = True)  
+    print('')
+    print('OBJECT')
+    print('---------------------------------------------------------------------------')
+    print(df.select_dtypes(['object']).apply(lambda x: len(set(x))), flush = True)  
+    return print('')
+
+# Impute Missing Values:
+class DataFrameImputer(TransformerMixin):
+
+    def __init__(self):
+        """Impute missing values.
+        Columns of dtype object are imputed with the most frequent value 
+        in column.
+        Columns of other types are imputed with mean of column.
+        """
+    def fit(self, X, y=None):
+
+        self.fill = pd.Series([X[c].value_counts().index[0]
+            if X[c].dtype == np.dtype('O') else X[c].mean() for c in X],
+            index=X.columns)
+
+        return self
+
+    def transform(self, X, y=None):
+        return X.fillna(self.fill)
+    
+# Missing Values Imputation
+def scatter_matrix_all(frame, alpha=0.5, figsize=None, grid=False, diagonal='hist', marker='.', density_kwds=None, hist_kwds=None, range_padding=0.05, **kwds):
+
+    import matplotlib as mpl
+    import matplotlib.pyplot as plt
+    from matplotlib.artist import setp
+    import pandas.core.common as com
+    from pandas.compat import range, lrange, lmap, map, zip
+    from statsmodels.nonparametric.smoothers_lowess import lowess
+    
+    df = frame
+    num_cols = frame._get_numeric_data().columns.values
+    n = df.columns.size
+    fig, axes = plt.subplots(nrows=n, ncols=n, figsize=figsize, squeeze=False)
+
+    # no gaps between subplots
+    fig.subplots_adjust(wspace=0, hspace=0)
+
+    mask = com.notnull(df)
+    marker = _get_marker_compat(marker)
+
+    hist_kwds = hist_kwds or {}
+    density_kwds = density_kwds or {}
+
+    # workaround because `c='b'` is hardcoded in matplotlibs scatter method
+    kwds.setdefault('c', plt.rcParams['patch.facecolor'])
+
+    boundaries_list = []
+    for a in df.columns:
+        if a in num_cols:
+            values = df[a].values[mask[a].values]
+        else:
+            values = df[a].value_counts()
+        rmin_, rmax_ = np.min(values), np.max(values)
+        rdelta_ext = (rmax_ - rmin_) * range_padding / 2.
+        boundaries_list.append((rmin_ - rdelta_ext, rmax_+ rdelta_ext))
+
+    for i, a in zip(lrange(n), df.columns):
+        for j, b in zip(lrange(n), df.columns):
+            ax = axes[i, j]
+
+            if i == j:
+                if a in num_cols:    # numerical variable
+                    values = df[a].values[mask[a].values]
+                    # Deal with the diagonal by drawing a histogram there.
+                    if diagonal == 'hist':
+                        ax.hist(values, **hist_kwds)
+                    elif diagonal in ('kde', 'density'):
+                        from scipy.stats import gaussian_kde
+                        y = values
+                        gkde = gaussian_kde(y)
+                        ind = np.linspace(y.min(), y.max(), 1000)
+                        ax.plot(ind, gkde.evaluate(ind), **density_kwds)
+                    ax.set_xlim(boundaries_list[i])
+                else:                # categorical variable
+                    values = df[a].value_counts()
+                    ax.bar(list(range(df[a].nunique())), values)
+            else:
+                common = (mask[a] & mask[b]).values
+                # two numerical variables
+                if a in num_cols and b in num_cols:
+                    if i > j:
+                        ax.scatter(df[b][common], df[a][common], marker=marker, alpha=alpha, **kwds)
+                        # The following 2 lines add the lowess smoothing
+                        ys = lowess(df[a][common], df[b][common])
+                        ax.plot(ys[:,0], ys[:,1], 'red')
+                    else:
+                        pearR = df[[a, b]].corr()
+                        ax.text(df[b].min(), df[a].min(), 'r = %.4f' % (pearR.iloc[0][1]))
+                    ax.set_xlim(boundaries_list[j])
+                    ax.set_ylim(boundaries_list[i])
+                # two categorical variables
+                elif a not in num_cols and b not in num_cols:
+                    if i > j:
+                        from statsmodels.graphics import mosaicplot
+                        mosaicplot.mosaic(df, [b, a], ax, labelizer=lambda k:'')
+                # one numerical variable and one categorical variable
+                else:
+                    if i > j:
+                        tol = pd.DataFrame(df[[a, b]])
+                        if a in num_cols:
+                            label = [ k for k, v in tol.groupby(b) ]
+                            values = [ v[a].tolist() for k, v in tol.groupby(b) ]
+                            ax.boxplot(values, labels=label)
+                        else:
+                            label = [ k for k, v in tol.groupby(a) ]
+                            values = [ v[b].tolist() for k, v in tol.groupby(a) ]
+                            ax.boxplot(values, labels=label, vert=False)
+
+            ax.set_xlabel('')
+            ax.set_ylabel('')
+
+            _label_axis(ax, kind='x', label=b, position='bottom', rotate=True)
+            _label_axis(ax, kind='y', label=a, position='left')
+
+            if j!= 0:
+                ax.yaxis.set_visible(False)
+            if i != n-1:
+                ax.xaxis.set_visible(False)
+
+    for ax in axes.flat:
+        setp(ax.get_xticklabels(), fontsize=8)
+        setp(ax.get_yticklabels(), fontsize=8)
+    return fig
+    
+
+def _label_axis(ax, kind='x', label='', position='top', ticks=True, rotate=False):
+    from matplotlib.artist import setp
+    if kind == 'x':
+        ax.set_xlabel(label, visible=True)
+        ax.xaxis.set_visible(True)
+        ax.xaxis.set_ticks_position(position)
+        ax.xaxis.set_label_position(position)
+        if rotate:
+            setp(ax.get_xticklabels(), rotation=90)
+    elif kind == 'y':
+        ax.yaxis.set_visible(True)
+        ax.set_ylabel(label, visible=True)
+        #ax.set_ylabel(a)
+        ax.yaxis.set_ticks_position(position)
+        ax.yaxis.set_label_position(position)
+    return
+
+def _get_marker_compat(marker):
+    import matplotlib.lines as mlines
+    import matplotlib as mpl
+    if mpl.__version__ < '1.1.0' and marker == '.':
+        return 'o'
+    if marker not in mlines.lineMarkers:
+        return 'o'
+    return marker
+
+# PCA Dimensionality Reduction
+def pca_summary(pca, standardised_data, out=True):
+    names = ["PC"+str(i) for i in range(1, len(pca.explained_variance_ratio_)+1)]
+    a = list(np.std(pca.transform(standardised_data), axis=0))
+    b = list(pca.explained_variance_ratio_)
+    c = [np.sum(pca.explained_variance_ratio_[:i]) for i in range(1, len(pca.explained_variance_ratio_)+1)]
+    columns = pd.MultiIndex.from_tuples([("sdev", "Standard deviation"), ("varprop", "Proportion of Variance"), ("cumprop", "Cumulative Proportion")])
+    summary = pd.DataFrame(list(zip(a, b, c)), index=names, columns=columns)
+    if out:
+        print("Importance of components:")
+        display(summary)
+    return summary
+
+# Accumulative importance and number of components graph
+def screeplot(pca, standardised_values):
+    y = np.std(pca.transform(standardised_values), axis=0)**2
+    x = np.arange(len(y)) + 1
+    plt.plot(x, y, "o-")
+    plt.xticks(x, ["Comp."+str(i) for i in x], rotation=60)
+    plt.ylabel("Variance")
+    plt.show()
+
+# Graph any PCA of the components desired: "first" parameter indicates Component in x-axis
+# "second" parameter indicates Component in y-axis 
+def pca_scatter(pca, standardised_values, classifs, first, second):
+    foo = pca.transform(standardised_values)
+    bar = pd.DataFrame(list(zip(foo[:, first-1], foo[:, second-1], classifs)), columns=[str("PC"+str(first)), str("PC"+str(second)), "Class"])
+    sns.lmplot(str("PC"+str(first)), str("PC"+str(second)), bar, hue="Class", fit_reg=False)
