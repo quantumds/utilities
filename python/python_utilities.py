f184fbdf3fdf6d4782f53728cce28ad1fe7607dc
quantumds <19568551+quantumds@users.noreply.github.com>
2019-02-19 15:36:12 +0100

    Update python_utilities.py

--- python/python_utilities.py
+++ python/python_utilities.py
@@ -899,20 +899,17 @@ def pca_scatter(pca, standardised_values, classifs, first, second):
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.ticker import PercentFormatter
-
 df = pd.DataFrame({'country': [177.0, 7.0, 4.0, 2.0, 2.0, 1.0, 1.0, 1.0]})
 df.index = ['USA', 'Canada', 'Russia', 'UK', 'Belgium', 'Mexico', 'Germany', 'Denmark']
 df = df.sort_values(by='country',ascending=False)
 df["cumpercentage"] = df["country"].cumsum()/df["country"].sum()*100
 print(df)
-
 fig = plt.figure(figsize=(15, 15))
 ax = fig.add_subplot(1,1,1)
 ax.bar(df.index, df["country"], color="C0")
 ax2 = ax.twinx()
 ax2.plot(df.index, df["cumpercentage"], color="C1", marker="D", ms=7)
 ax2.yaxis.set_major_formatter(PercentFormatter())
-
 ax.tick_params(axis="y", colors="C0")
 ax2.tick_params(axis="y", colors="C1")
 plt.show()
