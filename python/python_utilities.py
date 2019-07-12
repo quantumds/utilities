74415b90f038aaa042531792b22577ae2388e32d
quantumds <19568551+quantumds@users.noreply.github.com>
2019-07-12 15:37:10 +0200

    Update python_utilities.py

--- python/python_utilities.py
+++ python/python_utilities.py
@@ -656,6 +656,21 @@ def _get_marker_compat(marker):
 ############################################################################
 # PRE-PROCESSING FUNCTIONS
 ############################################################################
+# Missing values
+def missing_values_table(df):
+        mis_val = df.isnull().sum()
+        mis_val_percent = 100 * df.isnull().sum() / len(df)
+        mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+        mis_val_table_ren_columns = mis_val_table.rename(
+        columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+        mis_val_table_ren_columns = mis_val_table_ren_columns[
+            mis_val_table_ren_columns.iloc[:,1] != 0].sort_values(
+        '% of Total Values', ascending=False).round(1)
+        print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"     
+            "There are " + str(mis_val_table_ren_columns.shape[0]) +
+              " columns that have missing values.")
+        return mis_val_table_ren_columns
+   
 # Frequency table of every variable:
 def freq(df):
     pd.options.display.max_rows = 10
